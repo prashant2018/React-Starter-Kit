2185a98808566426f662d37fe7ac7f79aa97b9e7
Prashant <prashant.kiit2018@gmail.com>
2019-05-07 00:49:15 +0530

    In memory zip create

--- server/index.js
+++ server/index.js
@@ -1,44 +1,31 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
-const Zip = require('jszip');
 const archiver = require('archiver');
-
 
 const app = express();
 const port = 3000;
-var zip = new Zip();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
 app.use(express.json());  
 
 app.get('/', (req, res) => {
     getReactApp("New-React-App", res);
 });
 
-app.post('/reactapp', (req, res) => {
-    name = req.body['name'];
+app.get('/reactapp', (req, res) => {
+    name = req.query.name;
+    fileName = name+'.zip';
+    res.writeHead(200,'Ok',{ 'Content-Type': 'application/zip','Content-disposition': 'attachment; filename='+fileName})
     getReactApp(name, res);
 });
 
 function getReactApp(name, res){
-    var fileName = __dirname + '/zipfiles/' + name + '.zip';
-    var output = fs.createWriteStream(fileName);
-    var archive = archiver('zip', {
-      zlib: { level: 9 }
-    });
-
-    output.on('close', function() {
-        console.log(archive.pointer() + ' total bytes');
-        console.log('archiver has been finalized and the output file descriptor has closed.');
-        res.download(fileName);
-    });
-
-    archive.pipe(output);
-    archive.append(fs.readFileSync('assets/package.json').toString().replace('react-starter-kit-placeholder',name), { name: 'package.json' });
-    archive.directory('assets/react_start_app/', false);
-    archive.finalize();
+    var zip = archiver('zip');
+    zip.pipe(res);
+    zip.append(fs.readFileSync('assets/package.json').toString().replace('react-starter-kit-placeholder',name), { name: 'package.json' });
+    zip.directory('assets/react_start_app/', false);
+    zip.finalize();
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
