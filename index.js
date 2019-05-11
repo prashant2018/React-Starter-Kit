1791d2361963a44be848e4bdfc46926e0e481a7d
Prashant <prashant.kiit2018@gmail.com>
2019-05-11 18:30:46 +0530

    Added scripts and icons folder for static serving

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(express.json());  
 
